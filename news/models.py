a1caa62f18b9a67c8f37c897b03dcbd317816375
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-22 19:08:31 +0300

    add date column

--- news/models.py
+++ news/models.py
@@ -21,4 +21,6 @@ class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
